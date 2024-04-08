7a41ba9bc2773bc72b91f602a10690a2c5ed058e
HannuSalmi1 <hannusalmi1@outlook.com>
2024-04-08 17:45:33 +0300

    images

--- src/App.js
+++ src/App.js
@@ -30,13 +30,13 @@ function App() {
 
             <img
               className="hannu"
-              src="./hannu_salmi_kuva.png"
+              src="./portfolio-app/hannu_salmi_kuva.png"
               alt="Image where Hannu Salmi looks happy"
             />
             {theme === "fiesta" && (
               <img
                 className="hat"
-                src="./sombreroHans.png"
+                src="./portfolio-app/sombreroHans.png"
                 alt="Image for Fiesta theme"
               />
             )}
